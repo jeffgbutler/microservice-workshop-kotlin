4df14c51aba5018c6b8f64481bb461d52cc499a5
Jeff Butler <jeffgbutler@gmail.com>
2019-12-11 16:22:16 -0500

    Remove unused import

--- solution/hystrix-dashboard/src/main/kotlin/microservice/workshop/hystrixdashboard/HystrixDashboardApplication.kt
+++ solution/hystrix-dashboard/src/main/kotlin/microservice/workshop/hystrixdashboard/HystrixDashboardApplication.kt
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.servlet.view.RedirectView
 
